71731f029e6b7fcd12e3c81ee8021afba17ad48b
Misha <Mishanya_05@bk.ru>
2023-09-13 13:51:21 +0300

    Начало 13.09.2023

---
+++ События/script.js
@@ -0,0 +1,38 @@
+// // const Emitter = require("events");
+// // let emitter = new Emitter();
+// // let eventName = "greet";
+// // emitter.on(eventName, function () {
+// //   console.log("Hello all!");
+// // });
+
+// // emitter.on(eventName, function () {
+// //   console.log("Привет!");
+// // });
+
+// // emitter.emit(eventName);
+
+// const Emitter = require("events");
+// let emitter = new Emitter();
+// let eventName = "greet";
+// emitter.on(eventName, function (data) {
+//   console.log(data);
+// });
+
+// emitter.emit(eventName, "Привет пир!");
+const EventEmitter = require("events");
+
+let eventName = "greet";
+
+class User extends EventEmitter {
+  sayHi(data) {
+    this.emit(eventName, data);
+  }
+}
+
+let user = new User();
+// добавляем к объекту user обработку события "greet"
+user.on(eventName, function (data) {
+  console.log(data);
+});
+
+user.sayHi("Мне нужна твоя одежда...");
---
+++ Создание сервера/script.js
@@ -0,0 +1,48 @@
+// // const http = require("http");
+
+// // http
+// //   .createServer(function (request, response) {
+// //     response.end("Hello world!");
+// //   })
+// //   .listen(3000);
+
+// // var http = require("http");
+
+// // http
+// //   .createServer(function (request, response) {
+// //     console.log("Url: " + request.url);
+// //     console.log("Тип запроса: " + request.method);
+// //     console.log("User-Agent: " + request.headers["user-agent"]);
+// //     console.log("Все заголовки");
+// //     console.log(request.headers);
+
+// //     response.end();
+// //   })
+// //   .listen(3000);
+
+// const http = require("http");
+
+// http
+//   .createServer(function (request, response) {
+//     response.setHeader("UserId", 12);
+//     response.setHeader("Content-Type", "text/html; charset=utf-8;");
+//     response.write("<h2>hello world</h2>");
+//     response.end();
+//   })
+//   .listen(3000);
+const http = require("http");
+
+http
+  .createServer(function (request, response) {
+    response.setHeader("Content-Type", "text/html");
+    response.write("<!DOCTYPE html>");
+    response.write("<html>");
+    response.write("<head>");
+    response.write("<title>Hello Node.js</title>");
+    response.write('<meta charset="utf-8" />');
+    response.write("</head>");
+    response.write("<body><h2>Привет миг</h2></body>");
+    response.write("</html>");
+    response.end();
+  })
+  .listen(3000);
