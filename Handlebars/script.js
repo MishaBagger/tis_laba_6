aea654d69fb29961f9093ff4e8d98134f294ae31
v0l0 <podolsky35alex@gmail.com>
2023-09-18 12:07:28 +0300

    18.09.2023 Kalinin

---
+++ Handlebars/script.js
@@ -0,0 +1,49 @@
+// const express = require('express');
+
+// const app = express();
+
+// app.set('view engine', 'hbs');
+
+// app.use('/contact', function (request, response) {
+//     response.render('contact.hbs', {
+//         title: 'Мои контакты',
+//         email: 'gavgav@mycorp.com',
+//         phone: '+1234567890',
+//     });
+// });
+// app.use('/', function (request, response) {
+//     response.send('Главная страница');
+// });
+// app.listen(3000);
+
+const express = require('express');
+
+const app = express();
+app.set('view engine', 'hbs');
+
+app.use('/contact', function (request, response) {
+    response.render('contact.hbs', {
+        title: 'Мои контакты',
+        emailsVisible: true,
+        emails: ['gavgav@mycorp.com', 'mioaw@mycorp.com'],
+        phone: '+1234567890',
+    });
+});
+
+app.use('/', function (request, response) {
+    response.send('Главная страница');
+});
+app.listen(3000);
+
+// const express = require('express');
+
+// const app = express();
+
+// app.set('view engine', 'hbs');
+// app.set('views', 'templates'); // установка пути к представлениям
+
+// app.use('/contact', function (request, response) {
+//     response.render('contact');
+// });
+
+// app.listen(3000);
